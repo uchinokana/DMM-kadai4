3f4d19842b7353a12d7211e07bf695425afced08
EC2 Default User <ec2-user@ip-172-31-15-221.ap-northeast-1.compute.internal>
2022-12-08 04:54:01 +0000

    エラー

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  get '/search', to: 'searches#search'
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
@@ -13,7 +14,6 @@ Rails.application.routes.draw do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
-    get "search" => "searches#search"
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For detget ails on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
